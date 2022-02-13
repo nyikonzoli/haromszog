33e273d1c674de1f5364f7404aefcb5edc067057
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-13 11:52:31 +0100

    szerkeszhetoreturntrue

--- haromszog/haromszogTest/HaromszogTeszteles.cs
+++ haromszog/haromszogTest/HaromszogTeszteles.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using haromszog;
 
 namespace haromszogTest
 {
@@ -6,8 +7,14 @@ namespace haromszogTest
     public class HaromszogTeszteles
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow(10, 10, 10)]
+        [DataRow(2, 9, 8)]
+        [DataRow(9823, 121, 9811)]
+        public void SzerkeszthetoReturnTrue(double a, double b, double c)
         {
+            Haromszog h = new Haromszog(a, b, c);
+            bool result = h.szerkesztheto();
+            Assert.IsTrue(result);
         }
     }
 }
