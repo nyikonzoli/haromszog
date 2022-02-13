5e38238555bbc60b515b3f548a1a986e859c3cd5
nyikonzoltanferenc <33204609+nyikonzoli@users.noreply.github.com>
2022-02-13 12:26:23 +0100

    terulettest

---
+++ haromszog/haromszogTest/HaromszogTerulet.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using haromszog;
+using System;
+
+namespace haromszogTest
+{
+    [TestClass]
+    public class HaromszogTerulet
+    {
+        [TestMethod]
+        [DataRow(10, 10, 10, 43.3)]
+        [DataRow(2, 9, 8, 7.3)]
+        [DataRow(9823, 121, 9811, 590989.3)]
+        public void TeruletCorrect(double a, double b, double c, double expected)
+        {
+            Haromszog h = new Haromszog(a, b, c);
+            double result = h.terulet();
+            Assert.AreEqual(Math.Round(result, 1), expected);
+        }
+
+        [TestMethod]
+        [DataRow(10, 10, 10, 43.3)]
+        [DataRow(2, 9, 8, 20)]
+        [DataRow(9823, 121, 9811, 0)]
+        public void TeruletIncorrect(double a, double b, double c, double expected)
+        {
+            Haromszog h = new Haromszog(a, b, c);
+            double result = h.terulet();
+            Assert.AreNotEqual(result, expected);
+        }
+    }
+}
